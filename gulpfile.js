9fb781983fef18ea504b9d38fe2496fa1a6b5c4a
Joedaimar G <gozonjoedaimar@gmail.com>
2018-08-11 12:21:30 +0800

    Updated package and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,51 @@
 var gulp = require('gulp');
 
 var clean = require('gulp-clean');
-// var jshint = require('gulp-jshint');
-// var concat = require('gulp-concat');
-// var uglify = require('gulp-uglify');
 var minify = require('gulp-minify');
-// var imagemin = require('gulp-imagemin');
+var zip = require('gulp-zip');
 
 var bases = {
- app: 'app/',
- dist: 'dist/',
- vendor: 'vendor/',
- node_modules: 'node_modules/',
+	dist: 'dist/',
+	vendor: 'vendor/',
+	node_modules: 'node_modules/',
 };
 
 var paths = {
- scripts: [
- 	'tac-stat-ob.js'
- 	],
- // libs: [
- // 	// 'scripts/libs/jquery/dist/jquery.js',
- // 	// 'scripts/libs/underscore/underscore.js',
- // 	// 'scripts/backbone/backbone.js'
- // 	bases.node_modules + 'bootstrap/**/*',
- // 	bases.node_modules + 'datatables.net/**/*',
- // 	bases.node_modules + 'datatables.net-bs4/**/*',
-	// ],
- // styles: ['styles/**/*.css'],
- // html: ['index.html', '404.html'],
- // images: ['images/**/*.png'],
- // extras: ['crossdomain.xml', 'humans.txt', 'manifest.appcache', 'robots.txt', 'favicon.ico'],
+	scripts: [
+		'tac-stat-ob.js'
+	],
 };
 
-// Delete the dist directory
+// Delete the dist and vendor directory
 gulp.task('clean', function() {
- return gulp.src([bases.vendor, bases.dist])
- .pipe(clean());
+	return gulp.src([bases.vendor, bases.dist])
+		.pipe(clean());
 });
 
-// Process scripts and concatenate them into one output file
-// gulp.task('scripts', ['clean'], function() {
-//  gulp.src(paths.scripts, {cwd: bases.app})
-//  .pipe(jshint())
-//  .pipe(jshint.reporter('default'))
-//  .pipe(uglify())
-//  .pipe(concat('app.min.js'))
-//  .pipe(gulp.dest(bases.dist + 'scripts/'));
-// });
-
-// Imagemin images and ouput them in dist
-// gulp.task('imagemin', ['clean'], function() {
-//  gulp.src(paths.images, {cwd: bases.app})
-//  .pipe(imagemin())
-//  .pipe(gulp.dest(bases.dist + 'images/'));
-// });
-
-// Copy all other files to dist directly
+// Copy node files to dist directly
 gulp.task('copy', ['clean'], function() {
- // Copy html
- // gulp.src(paths.html, {cwd: bases.app})
- // .pipe(gulp.dest(bases.dist));
+	/* Copy bootstrap */
+	gulp.src(bases.node_modules + 'bootstrap/**/*')
+		.pipe(gulp.dest(bases.vendor + 'bootstrap'));
 
- // Copy styles
- // gulp.src(paths.styles, {cwd: bases.app})
- // .pipe(gulp.dest(bases.dist + 'styles'));
+	/* Copy datatables.net */
+	gulp.src(bases.node_modules + 'datatables.net/**/*')
+		.pipe(gulp.dest(bases.vendor + 'datatables.net'));
 
- // Copy lib scripts, maintaining the original directory structure
- // gulp.src(paths.libs, {cwd: './'})
- // .pipe(gulp.dest(bases.vendor));
- gulp.src(bases.node_modules + 'bootstrap/**/*', {cwd: './'})
- .pipe(gulp.dest(bases.vendor + 'bootstrap'));
- 
- gulp.src(bases.node_modules + 'datatables.net/**/*', {cwd: './'})
- .pipe(gulp.dest(bases.vendor + 'datatables.net'));
-
- gulp.src(bases.node_modules + 'datatables.net-bs4/**/*', {cwd: './'})
- .pipe(gulp.dest(bases.vendor + 'datatables.net-bs4'));
-
- // Copy extra html5bp files
- // gulp.src(paths.extras, {cwd: bases.app})
- // .pipe(gulp.dest(bases.dist));
+	/* Copy datatables.net-bs4 */
+	gulp.src(bases.node_modules + 'datatables.net-bs4/**/*')
+		.pipe(gulp.dest(bases.vendor + 'datatables.net-bs4'));
 });
 
 gulp.task('minify', ['clean'], function() {
-	gulp.src(paths.scripts, {cwd: './'})
+	gulp.src(paths.scripts)
 		.pipe(minify())
 		.pipe(gulp.dest(bases.dist))
 });
 
-// A development task to run anytime a file changes
-gulp.task('watch', function() {
- gulp.watch('app/**/*', ['scripts', 'copy']);
-});
-
 // Define the default task as a sequence of the above tasks
-gulp.task('default', ['copy', 'minify']);
+gulp.task('default', ['copy', 'minify'], function() {
+	gulp.src('./')
+		.pipe(zip('tac-quick-stat-view.zip'))
+		.pipe(gulp.dest(bases.dist));
+});
